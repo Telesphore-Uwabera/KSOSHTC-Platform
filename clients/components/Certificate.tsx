267bd21129dc470c2a2977930404731e86a4241f
Telesphore Uwabera <t.uwabera@alustudent.com>
2026-04-13 11:52:59 +0200

    feat: replace old stamp with high-res logo and apply multiply blend for transparency

--- clients/components/Certificate.tsx
+++ clients/components/Certificate.tsx
@@ -109,7 +109,7 @@ export const Certificate: React.FC<CertificateProps> = ({ data }) => {
           <div className="flex flex-col items-center justify-center w-[30%] min-h-[200px]">
              <div className="z-20 pointer-events-none flex items-center justify-center" style={{ width: '180px', height: '180px' }}>
                 <img 
-                   src="/certificate/stamp.webp" 
+                   src="/certificate/ksohtc-logo.webp" 
                    alt="Stamp" 
                    className="w-[180px] h-[180px]"
                    style={{ 
@@ -117,7 +117,8 @@ export const Certificate: React.FC<CertificateProps> = ({ data }) => {
                      height: '180px', 
                      maxWidth: '180px', 
                      maxHeight: '180px',
-                     objectFit: 'contain'
+                     objectFit: 'contain',
+                     mixBlendMode: 'multiply'
                    }} 
                 />
              </div>
